36ba71948cb1734061c3fd9e82c0dbba4b39ea30
hana cho <sjfn242@gmail.com>
2024-01-15 22:25:33 +0900

    initial setting

---
+++ src/main/java/zb/accountMangement/AccountMangementApplication.java
@@ -0,0 +1,13 @@
+package zb.accountMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zb/accountMangement/AccountMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package zb.accountMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
